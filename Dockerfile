86d39d3556a3b23c842c176196ad6fb2fc13164e
caoli5288 <caoli5288@gmail.com>
2020-11-17 11:41:11 +0800

    update libtorrent

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ FROM alpine
 
 RUN apk add --no-cache shadow libstdc++ qt5-qtbase
 
-COPY --from=builder /usr/local/lib/libtorrent-rasterbar.so.1.2.10 /usr/local/lib/libtorrent-rasterbar.so.10
+COPY --from=builder /usr/local/lib/libtorrent-rasterbar.so.1.2.11 /usr/local/lib/libtorrent-rasterbar.so.11
 
 COPY --from=builder /usr/local/bin/qbittorrent-nox /usr/local/bin/qbittorrent-nox
 
--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 apk add make cmake g++ openssl-dev boost-dev qt5-qtbase-dev qt5-qttools-dev
 
-wget https://github.com/arvidn/libtorrent/releases/download/libtorrent-1.2.10/libtorrent-rasterbar-1.2.10.tar.gz
+wget https://github.com/arvidn/libtorrent/releases/download/libtorrent-1.2.10/libtorrent-rasterbar-1.2.11.tar.gz
 
-tar xvf libtorrent-rasterbar-1.2.10.tar.gz
+tar xvf libtorrent-rasterbar-1.2.11.tar.gz
 
-cd libtorrent-rasterbar-1.2.10/
+cd libtorrent-rasterbar-1.2.11/
 
 mkdir build && cd build/
 
 cmake -DCMAKE_INSTALL_LIBDIR=lib ..
 
-make && make install && strip /usr/local/lib/libtorrent-rasterbar.so.1.2.10
+make && make install && strip /usr/local/lib/libtorrent-rasterbar.so.1.2.11
 
 wget https://github.com/qbittorrent/qBittorrent/archive/master.zip
 
