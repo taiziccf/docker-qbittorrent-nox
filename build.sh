2ad9cdbf493d07d60d340fbb874f88e118e63379
taiziccf <taiziccf@gmail.com>
2020-12-23 14:49:59 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,17 +2,17 @@
 
 apk add make cmake g++ openssl-dev boost-dev qt5-qtbase-dev qt5-qttools-dev
 
-wget https://github.com/arvidn/libtorrent/releases/download/v1.2.11/libtorrent-rasterbar-1.2.11.tar.gz
+wget https://github.com/arvidn/libtorrent/archive/RC_2_0.zip
 
-tar xvf libtorrent-rasterbar-1.2.11.tar.gz
+unzip RC_2_0.zip
 
-cd libtorrent-rasterbar-1.2.11/
+cd libtorrent-RC_2_0/
 
 mkdir build && cd build/
 
 cmake -DCMAKE_INSTALL_LIBDIR=lib ..
 
-make && make install && strip /usr/local/lib/libtorrent-rasterbar.so.1.2.11
+make && make install && strip /usr/local/lib/libtorrent-rasterbar.so.2.0
 
 wget https://github.com/qbittorrent/qBittorrent/archive/master.zip
 
